818cc64c386eda9985abcb6dd2210ac3896aca5b
Vcg2007 <vxmp2007@gmail.com>
2021-03-24 09:16:32 -0300

    criação do respositorio de clone netflix

---
+++ js/owl/setup.js
@@ -0,0 +1,37 @@
+/*$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+*/
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
